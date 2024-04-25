3a9319e3eae93745c639f46df49b2af6f0c2717d
TripleKdev <80637250+kangkengkhadev@users.noreply.github.com>
2024-04-25 13:31:51 +0700

    add freeze

---
+++ src/logic/FreezePotion.java
@@ -0,0 +1,7 @@
+package logic;
+
+public class FreezePotion extends Item {
+    public FreezePotion(double x, double y, double width, double height) {
+        super(x, y, width, height, "");
+    }
+}
