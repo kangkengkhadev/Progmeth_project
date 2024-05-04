32a2a157139c0d820f2acc03e48c2321cbd1b4c1
K1EW <weerawatwmt@gmail.com>
2024-05-05 01:34:13 +0700

    remove magic number from RespawnState

--- src/logic/entity/ghost/state/RespawnState.java
+++ src/logic/entity/ghost/state/RespawnState.java
@@ -3,6 +3,7 @@ package logic.entity.ghost.state;
 import logic.fsm.BaseState;
 import logic.entity.ghost.BaseGhost;
 import logic.Vector2D;
+import util.Config;
 
 public class RespawnState extends BaseState {
     private BaseGhost ghost;
@@ -13,14 +14,14 @@ public class RespawnState extends BaseState {
 
     @Override
     public void update(double delta) {
-        if (ghost.getPosition().getX() == 23 && ghost.getPosition().getY() == 10) {
+        if (ghost.getPosition().getX() == Config.GHOST_X_ORIGIN && ghost.getPosition().getY() == Config.GHOST_Y_ORIGIN) {
             ghost.getFsm().changeState(new SpawnState(ghost));
         }
     }
 
     @Override
     public void onEnter() {
-        Vector2D newTarget = new Vector2D(23, 10);
+        Vector2D newTarget = new Vector2D(Config.GHOST_X_ORIGIN, Config.GHOST_Y_ORIGIN);
         ghost.setTarget(newTarget);
     }
 
